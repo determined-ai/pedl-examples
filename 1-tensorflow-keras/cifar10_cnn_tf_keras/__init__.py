1336fea66e6b99e7faf3687447def5118b9ca44e
Sean Rowan <sean@determined.ai>
2020-01-13 09:04:17 -0800

    remove old

--- 1-tensorflow-keras/cifar10_cnn_tf_keras/__init__.py
+++
@@ -1,2 +0,0 @@
-# from model_def import CIFARTrial, make_data_loaders
-from model_def import CIFARTrial, make_data_loaders
--- 2-pytorch/cifar10_cnn_pytorch/__init__.py
+++
@@ -1,2 +0,0 @@
-from data import download_data, make_data_loaders
-from model_def import CIFARTrial
